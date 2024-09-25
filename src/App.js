4b6999f81ec6b1f54cef36b372a4d7522f857778
extinctmule <extinctmule@gmail.com>
2024-09-25 10:54:40 +0900

    Update: Part1-기초

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  let post = "강남 우동 맛집";
+  const [postTitle, setPostTitle] = useState("남자 코트 추천");
+
+  let num = [1, 2];
+  setPostTitle("f");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <div class="black-nav">
+          <h4 style={{ fontSize: "20px" }}>블로그</h4>
+        </div>
+        <div className="list">
+          <h4>글제목</h4>
+          <p>글내용</p>
+        </div>
+        <div className="list">
+          <h4>{postTitle}</h4>
+          <p>글내용</p>
+        </div>
+      </div>
     </div>
   );
 }
