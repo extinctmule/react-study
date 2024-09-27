4244bdf020fddb6ff86a3f20b269084a00c7ed53
extinctmule <extinctmule@gmail.com>
2024-09-27 11:45:08 +0900

    Update: 모달 동적인UI로 변경

--- src/App.js
+++ src/App.js
@@ -7,14 +7,13 @@ function App() {
     "판교 덮밥 맛집",
     "파이썬 독학",
   ]);
-
   const [contents, setContents] = useState([
     "생물학과",
     "유스퀘이스 지하1층에 있어요.",
     "할만 해요.",
   ]);
-
   const [likes, setLikes] = useState(0);
+  const [modal, setModal] = useState(false);
 
   const onClickLikes = () => {
     setLikes(likes + 1);
@@ -40,10 +39,16 @@ function App() {
         <p>{contents[1]}</p>
       </div>
       <div className="list">
-        <h4>{title[2]}</h4>
+        <h4
+          onClick={() => {
+            setModal(!modal);
+          }}
+        >
+          {title[2]}
+        </h4>
         <p>{contents[2]}</p>
       </div>
-      <Modal />
+      {modal ? <Modal /> : null}
     </div>
   );
 }
